b5175a9f8905be6800050fa28ef2932238d4682b
8eu4 <asherh113@gmail.com>
2026-01-02 14:21:50 +0700

    vision

--- Assets/VisionMode.cs
+++ Assets/VisionMode.cs
@@ -8,12 +8,18 @@ public class VisionMode : MonoBehaviour
     public float normalEmission = 0f;
     public float visionEmission = 5f;
     private bool isVisionOn = false;
+    public Camera HighlightCamera;
 
     [System.Serializable]
     public class HighlightTarget
     {
         public Material material;
         public Color visionColor = Color.cyan; // color when vision mode is on
+    }
+
+    private void Start()
+    {
+        HighlightCamera.transform.GetComponent<Camera>().enabled = false;
     }
 
     void Update()
@@ -36,11 +42,13 @@ public class VisionMode : MonoBehaviour
             if (isVisionOn)
             {
                 // Use bright vibrant emission color
+                HighlightCamera.transform.GetComponent<Camera>().enabled = true;
                 target.material.SetColor("_EmissionColor", target.visionColor * visionEmission);
             }
             else
             {
                 // Turn emission off
+                HighlightCamera.transform.GetComponent<Camera>().enabled = false;
                 target.material.SetColor("_EmissionColor", target.visionColor * normalEmission);
             }
         }
