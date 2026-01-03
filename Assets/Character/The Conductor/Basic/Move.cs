306b05c2d2e4865f9989754d49a9f167e7444654
8eu4 <asherh113@gmail.com>
2026-01-03 13:11:18 +0700

    lompat

--- Assets/Character/The Conductor/Basic/Move.cs
+++ Assets/Character/The Conductor/Basic/Move.cs
@@ -4,12 +4,14 @@ public class Move : MonoBehaviour
 {
     public float speed;
     public float groundDrag;
-
+    
+    public float jumpForce = 5f;
+    public float jumpCooldown = 0f;
+    bool readyToJump = true;
 
     public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
-
 
     Transform orientation;
 
@@ -23,21 +25,22 @@ public class Move : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-        orientation = GameObject.FindGameObjectWithTag("Player").transform.Find("Orientation").transform;
+        orientation = transform.Find("Orientation");
     }
+
     void Update()
     {
         // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); 
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        print(gameObject.name + " " + grounded + " grounded");
 
         myInput();
         speedControl();
 
         // handle drag
-        if (grounded)
-            rb.linearDamping = groundDrag;
-        else
-            rb.linearDamping = 0;
+        rb.linearDamping = grounded ? groundDrag : 0;
+
+        print("ready to jump bool: " + readyToJump);
     }
 
     void FixedUpdate()
@@ -49,14 +52,31 @@ public class Move : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space) && readyToJump && grounded && gameObject.CompareTag("Player")) 
+        {
+            print(gameObject.name + " ready to jump");
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     void movePlayer()
     {
-        if (gameObject.tag != "Player") return;
-        orientation = GameObject.FindGameObjectWithTag("Player").transform.Find("Orientation").transform;
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection.normalized * speed * 10f, ForceMode.Force);
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true;
     }
 
     void speedControl()
@@ -69,5 +89,4 @@ public class Move : MonoBehaviour
             rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
         }
     }
-
 }
