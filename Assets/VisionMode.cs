0f37c1d44cdec327d9e0e769f09ed8998b73853f
8eu4 <asherh113@gmail.com>
2026-01-05 14:41:19 +0700

    aku menyerah

--- Assets/VisionMode.cs
+++ Assets/VisionMode.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class VisionMode : MonoBehaviour
 {
@@ -8,18 +9,12 @@ public class VisionMode : MonoBehaviour
     public float normalEmission = 0f;
     public float visionEmission = 5f;
     private bool isVisionOn = false;
-    public Camera HighlightCamera;
 
     [System.Serializable]
     public class HighlightTarget
     {
         public Material material;
         public Color visionColor = Color.cyan; // color when vision mode is on
-    }
-
-    private void Start()
-    {
-        HighlightCamera.transform.GetComponent<Camera>().enabled = false;
     }
 
     void Update()
@@ -42,13 +37,11 @@ public class VisionMode : MonoBehaviour
             if (isVisionOn)
             {
                 // Use bright vibrant emission color
-                HighlightCamera.transform.GetComponent<Camera>().enabled = true;
                 target.material.SetColor("_EmissionColor", target.visionColor * visionEmission);
             }
             else
             {
                 // Turn emission off
-                HighlightCamera.transform.GetComponent<Camera>().enabled = false;
                 target.material.SetColor("_EmissionColor", target.visionColor * normalEmission);
             }
         }
